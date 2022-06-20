3ce36782ba88fc6e7dcfded7256db4946ce5c7c5
priyajha <priya2302.pj@gmail.com>
2022-06-20 22:17:34 +0530

    Updates

---
+++ assets/skillsData.js
@@ -0,0 +1,42 @@
+export const skillList = [
+    {
+        skillName: 'C++',
+        skillUrl: './assets/C++.png',
+    },
+    {
+        skillName: 'Java',
+        skillUrl: './assets/java.gif',
+    },
+    {
+        skillName: 'JavaScript',
+        skillUrl: './assets/js.gif',
+    },
+    {
+        skillName: 'MySQL',
+        skillUrl: './assets/mysql.gif',
+    },
+    // {
+    //     skillName: 'Redux',
+    //     skillUrl: 'https://madhavbahl.tech/portfolio-assets/redux.png',
+    // },
+    // {
+    //     skillName: 'NodeJS',
+    //     skillUrl: 'https://madhavbahl.tech/portfolio-assets/node.png',
+    // },
+    // {
+    //     skillName: 'TypeScript',
+    //     skillUrl: 'https://madhavbahl.tech/portfolio-assets/ts.png',
+    // },
+    // {
+    //     skillName: 'Java',
+    //     skillUrl: 'https://madhavbahl.tech/portfolio-assets/java.png',
+    // },
+    // {
+    //     skillName: 'C#',
+    //     skillUrl: 'https://madhavbahl.tech/portfolio-assets/cs.png',
+    // },
+    // {
+    //     skillName: 'MongoDB',
+    //     skillUrl: 'https://madhavbahl.tech/portfolio-assets/mongo.png',
+    // },
+];
